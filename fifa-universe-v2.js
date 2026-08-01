fa2be520050b6244eb1830c11e3d92743bcdc986
qhse2026 <cglrcnttr@gmail.com>
2026-08-01 13:07:54 +0300

    ORUC_REIS_FIFA_UNIVERSE_V4_0_2_VISUAL_INTELLIGENCE_SAFE_CORE

--- fifa-universe-v2.js
+++ fifa-universe-v2.js
@@ -2,7 +2,7 @@
   "use strict";
 
   const VERSION = "3.2.1";
-  const BUILD = "321000";
+  const BUILD = "402000";
   const ROUTES = new Set(["dashboard", "livehub", "tournaments", "playershub", "recordshub", "mediahub", "adminhub"]);
   let mode = localStorage.getItem("fifa-universe-v2-mode") || "spectator";
   let selectedPlayer = localStorage.getItem("fifa-universe-v2-player") || "";
